0cddd21909b18e6e7124c4567a80228e27478873
Nguyen Truong Vu <vu.nguyen4951@gmail.com>
2025-05-17 22:45:07 +0700

    xoa profile

--- back-end/src/main/java/com/example/jobportal/repository/NotificationRepository.java
+++ back-end/src/main/java/com/example/jobportal/repository/NotificationRepository.java
@@ -3,6 +3,6 @@ package com.example.jobportal.repository;
 import com.example.jobportal.model.entity.Notifications;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotificationsRepository extends JpaRepository<Notifications, Integer> {
+public interface NotificationRepository extends JpaRepository<Notifications, Integer> {
 
 }
