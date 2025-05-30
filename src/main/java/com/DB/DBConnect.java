549127ac43392a4cd8587dbdb3209301fdea6a19
Nguyễn Hải Thiện <107566774+adamwhite625@users.noreply.github.com>
2025-05-30 14:28:25 +0700

    Update DBConnect.java

--- src/main/java/com/DB/DBConnect.java
+++ src/main/java/com/DB/DBConnect.java
@@ -4,20 +4,21 @@ import java.sql.*;
 
 public class DBConnect {
 
-	private static Connection conn;
-	
-	public static Connection getConn() {
-		
-		try {
-			if(conn == null) {
-				Class.forName("com.mysql.cj.jdbc.Driver");
-				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/job_portal", "root", "");
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return conn;
-	}
-	
+    private static Connection conn;
+
+    public static Connection getConn() {
+        try {
+            if (conn == null || conn.isClosed()) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                String url = "jdbc:mysql://turntable.proxy.rlwy.net:33735/railway?useSSL=false&serverTimezone=UTC";
+                String username = "root";
+                String password = "fbXkWtHycVilvoqshuwnbcqgJMXzZvEw";
+
+                conn = DriverManager.getConnection(url, username, password);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
