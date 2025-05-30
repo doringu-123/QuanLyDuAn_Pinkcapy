0f1c95a63c3557958c253c254cc3f1a29823dee9
Nguyễn Hải Thiện <107566774+adamwhite625@users.noreply.github.com>
2025-05-30 13:57:44 +0700

    Delete back-end-2/auth directory

--- back-end-2/auth/src/main/java/com/example/demo/dto/ApplicationRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dto;
-
-import lombok.Data;
-
-@Data
-public class ApplicationRequest {
-    private Integer resumeId;
-    private Integer jobId;
-}
--- back-end-2/auth/src/main/java/com/example/demo/repository/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ProfileRepository extends JpaRepository<Profile, Integer> {
-    Optional<Profile> findByUserUserId(Integer userId);
-}
